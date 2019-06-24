1f68defd393bb700587ce967e8d1c40ef7af19b9
aaroncustodio <custodio.aaron.1197@gmail.com>
2019-06-24 16:44:15 +0800

    added supplier entity and 1st integration test

--- QuickReach.ECommerce.Infra.Data.Tests/SupplierRepositoryTest.cs
+++ QuickReach.ECommerce.Infra.Data.Tests/SupplierRepositoryTest.cs
@@ -13,7 +13,26 @@ namespace QuickReach.ECommerce.Infra.Data.Tests
         [Fact]
         public void Create_WithValidSupplier_ShouldAddSupplierToDatabase()
         {
+            //arrange
+            var context = new ECommerceDbContext();
+            var sut = new SupplierRepository(context);
+            var supplier = new Supplier
+            {
+                Name = "Annie's Sweets",
+                Description = "We make your childhood sweeter!",
+                IsActive = true
+            };
 
+            //act
+            sut.Create(supplier);
+
+            //assert
+            var actual = sut.Retrieve(supplier.ID);
+            Assert.NotNull(actual);
+            Assert.True(actual.ID != 0);
+
+            //cleanup
+            sut.Delete(actual.ID);
         }
     }
 }
