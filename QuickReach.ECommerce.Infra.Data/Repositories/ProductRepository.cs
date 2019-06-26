b00d26436dfb4ffa5ddbda0afb705a7696b80bf5
aaroncustodio <custodio.aaron.1197@gmail.com>
2019-06-26 13:59:06 +0800

    Product controller

--- QuickReach.ECommerce.Infra.Data/Repositories/ProductRepository.cs
+++ QuickReach.ECommerce.Infra.Data/Repositories/ProductRepository.cs
@@ -2,19 +2,32 @@
 using QuickReach.ECommerce.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace QuickReach.ECommerce.Infra.Data.Repositories
 {
     public class ProductRepository
         : RepositoryBase<Product>,
-          IRepository<Product>
+          IProductRepository
     {
         public ProductRepository(
             ECommerceDbContext context)
             : base(context)
         {
 
+        }
+
+        public IEnumerable<Product> Retrieve(string search = "", int skip = 0, int count = 10)
+        {
+            var result = this.context.Products
+                .Where(c => c.Name.Contains(search) ||
+                            c.Description.Contains(search))
+                .Skip(skip)
+                .Take(count)
+                .ToList();
+
+            return result;
         }
     }
 }
